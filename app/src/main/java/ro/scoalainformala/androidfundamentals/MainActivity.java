8ca95231cc4b9386457b094dac2fa0e103cf466f
Arthur Nagy <arthur.93.nagy@gmail.com>
2021-05-25 10:13:32 +0300

    Course nr. 2 - Views

--- app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
+++ app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
@@ -3,6 +3,10 @@ package ro.scoalainformala.androidfundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
