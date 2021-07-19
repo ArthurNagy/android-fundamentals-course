38663252f08b56c2dad8be9289939b6b626eff89
Arthur Nagy <arthur.93.nagy@gmail.com>
2021-07-19 20:58:10 +0300

    Course nr. 7 - Examples

--- app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
+++ app/src/main/java/ro/scoalainformala/androidfundamentals/MainActivity.java
@@ -1,18 +1,49 @@
 package ro.scoalainformala.androidfundamentals;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingPermission")
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                    displayCurrentUserLocation();
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                    handlePermissionDenied();
+                }
+            });
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +75,44 @@ public class MainActivity extends AppCompatActivity {
             drawerLayout.close();
             return true;
         });
+
+
+        CoordinatorLayout coordinator = findViewById(R.id.coordinator);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> Snackbar.make(coordinator, "FAB was clicked", Snackbar.LENGTH_INDEFINITE).show());
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request location permission
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
+        } else {
+            // We already have location
+            displayCurrentUserLocation();
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+    private void displayCurrentUserLocation() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                // Logic to handle location object
+                TextView locationText = findViewById(R.id.location);
+                locationText.setText(location.toString());
+            }
+        });
+    }
+
+    private void handlePermissionDenied() {
+        CoordinatorLayout coordinator = findViewById(R.id.coordinator);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        Snackbar.make(coordinator, R.string.no_location_error, Snackbar.LENGTH_LONG)
+                .setAnchorView(bottomNavigationView)
+                .show();
     }
 
 }
